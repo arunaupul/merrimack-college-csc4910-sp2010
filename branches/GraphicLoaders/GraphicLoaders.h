c3f66b24e2307183d5bb6b3fe7ffc071c1296a6d
mikeloverme <mikeloverme@06d672e2-d70c-11de-b7b1-656321ec08b8>
2010-03-21 20:07:33 +0000

    chnaged it to use auto instead of always RGBA

--- branches/GraphicLoaders/GraphicLoaders.h
+++ branches/GraphicLoaders/GraphicLoaders.h
@@ -9,6 +9,7 @@ namespace GraphicLoaders
 	bool LoadNewBitmap( const std::wstring & fileName , TextureIdentifier & textureId );
 	bool LoadTga( const std::wstring & fileName , TextureIdentifier & textureId );
 	bool LoadTga( const std::string & fileName , TextureIdentifier & textureId );
+	bool LoadTgaBg( const std::string & fileName , TextureIdentifier & textureId );
 }
 
 #endif /* _GRAPHIC_LOADERS_H_ */
