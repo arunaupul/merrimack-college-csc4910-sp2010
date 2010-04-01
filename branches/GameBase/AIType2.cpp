bffa8dff3bd70a7fb5ae2a339c5fcdee3aa5893f
rfleming71 <rfleming71@06d672e2-d70c-11de-b7b1-656321ec08b8>
2010-04-01 23:24:36 +0000

    Removing unneed code

--- branches/GameBase/AIType2.cpp
+++ branches/GameBase/AIType2.cpp
@@ -1,12 +1,4 @@
 #include "AIType2.h"
-#include "ScoreManager.h"
-#include "GameDude.h"
-
-#define TRIGGER_DISTANCE	2
-#define LEFT				true
-#define RIGHT				false
-#define MOVE_DISTANCE		0.005
-#define FALL_SPEED			0.03
 
 AIType2::AIType2( const Square & startingPos , unsigned int leftTextureId , unsigned int rightTextureId )
 :	AIType1( startingPos , leftTextureId , rightTextureId ),
